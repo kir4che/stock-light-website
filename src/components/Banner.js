de93418e1220980efb6efcdaa9474a366d2c1c9f
kir4che <mollydcxxiii@gmail.com>
2023-05-18 20:10:31 +0800

    更新 Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,16 @@
+import Temple from '@/components/Temple'
+
 export default function Banner() {
 	return (
-		<div className='w-full'>
-			<img src='../images/banner.png' />
-			{/* molly 你看的到我改這行嗎 */}
+		<div className='w-full text-center border-b-2 h-96 border-b-text_black'>
+			<Temple />
+			<div id='banner-title'>
+				<span id='股'>股</span>
+				<span id='市'>市</span>
+				<span id='光'>光</span>
+				<span id='明'>明</span>
+				<span id='燈'>燈</span>
+			</div>
 		</div>
 	)
 }
