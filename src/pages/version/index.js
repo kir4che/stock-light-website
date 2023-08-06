2ac052ba7c3ecbab7ef4ba3ad337b0bdc25a1928
Wojolan <109399275+aa901002x@users.noreply.github.com>
2023-08-07 00:35:17 +0800

    更新版本資訊
    我要回歸了QQ派謝 7月都在讀書
    接下來我也會試著慢慢做幫忙排版~

--- src/pages/version/index.js
+++ src/pages/version/index.js
@@ -1,3 +1,22 @@
 export default function Version() {
-	return <></>
+	return (
+		<div className='max-w-screen-sm px-8 py-20 mx-auto lg:px-0'>
+				<h2 className='mb-5 font-medium tracking-tight text-center pb-2.5  space-x-2 border-b-[3px] border-b-primary_yellow'>版本更新資訊</h2>
+						<ul className='space-y-3'>
+							<li>
+								<h5 className="text-center font-bold ">Version 0.2</h5>
+								<img className='mt-5 w-full mb-5' src='https://fakeimg.pl/640x320/' alt='股市光明燈' />
+								<p className="mb-5 text-center text-base font-light">banner動畫設計</p>
+								<hr></hr>
+							</li>
+							<li>
+								<h5 className="text-center font-bold ">Version 0.1</h5>
+								<img className='mt-5 w-full mb-5' src='https://fakeimg.pl/640x320/' alt='股市光明燈' />
+								<p className="mb-5 text-center text-base font-light">確認網站的架構以及主色調</p>
+								<hr></hr>
+							</li>
+						</ul>
+				</div>
+	)
+	
 }
