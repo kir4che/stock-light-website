8edad99dac4f133f8da817b27217e8b70baaf7b9
kir4che <mollydcxxiii@gmail.com>
2023-08-19 23:34:54 +0800

    Update Marquee.js

--- src/components/Home/Marquee/Marquee.js
+++ src/components/Home/Marquee/Marquee.js
@@ -4,7 +4,7 @@ export default function Marquee() {
 
 	return (
 		<div className='flex items-center py-1 text-zinc-800 border-t-[1.35px] border-b-[1.35px] border-zinc-800 dark:border-t-[0.85px] dark:border-b-[0.85px] dark:border-white dark:text-white'>
-			<marquee className='flex-grow space-x-4' behavior='scroll' direction='left'>
+			<marquee behavior='scroll' direction='left'>
 				{news.map((item, index) => (
 					<span key={index} className='inline-block text-sm'>
 						{item}
