e2447f88d5cdecea313d9814b9fe9d536951d514
kir4che <mollydcxxiii@gmail.com>
2023-08-20 11:11:09 +0800

    Update GanttCarousel.js

--- src/components/About/GanttCarousel/GanttCarousel.js
+++ src/components/About/GanttCarousel/GanttCarousel.js
@@ -6,7 +6,7 @@ export default function GanttCarousel() {
 		<Carousel
 			indicators={false}
 			cycleNavigation={false}
-			className='w-[780px] mx-auto rounded-xl dark:bg-white h-[430px] flex items-center justify-center'
+			className='w-[780px] mx-auto rounded-xl dark:bg-white h-[400px] flex items-center justify-center'
 		>
 			<Image src='/images/development-timeline-1.svg' fill={true} alt='development-timeline-1' />
 			<Image src='/images/development-timeline-2.svg' fill={true} alt='development-timeline-2' />
