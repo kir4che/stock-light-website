3724cf6c82f0015eac2ba07c13c1dcd405f8d4e5
Wojolan <109399275+aa901002x@users.noreply.github.com>
2023-09-03 23:42:46 +0800

    Update Lantern.js

--- src/components/Light/Lantern.js
+++ src/components/Light/Lantern.js
@@ -4,97 +4,111 @@ export default function Lantern() {
 	return (
 		<div className='float'>
 			<Link href='/light/checkout'>
-				<div className='lantern lanternone'>
+				<div className='lantern lanterntag_container lanternone'>
 					<div className='laternlight'></div>
 					<div className='rounded-t-lg left rounded-b-md '></div>
 					<div className='rounded-t-lg right rounded-b-md '></div>
 					<div className='flame'></div>
-					<div className='flex justify-center w-8 mx-2 my-5 laterntag'>電子股</div>
+					<div className='flex justify-center w-8 mx-2 my-5 laterntag'>水泥股</div>
 				</div>
 			</Link>
-			<div className='lantern lanterntwo'>
+			<div className='lantern lanterntag_container lanterntwo'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>食品股</div>
 			</div>
-			<div className='lantern lanternthree'>
+			<div className='lantern lanterntag_container lanternthree'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>塑膠股</div>
 			</div>
-			<div className='lantern lanternfour'>
+			<div className='lantern lanterntag_container lanternfour'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>化學股</div>
 			</div>
-			<div className='lantern lanternfive'>
+			<div className='lantern lanterntag_container lanternfive'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>生技醫療股</div>
 			</div>
-			<div className='lantern lanternsix'>
+			<div className='lantern lanterntag_container lanternsix'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>鋼鐵股</div>
 			</div>
-			<div className='lantern lanternseven'>
+			<div className='lantern lanterntag_container lanternseven'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>汽車股</div>
 			</div>
-			<div className='lantern lanterneight'>
+			<div className='lantern lanterntag_container lanterneight'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>電機股</div>
 			</div>
-			<div className='lantern lanternnight'>
+			<div className='lantern lanterntag_container lanternnight'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>半導體股</div>
 			</div>
-			<div className='lantern lanternten'>
+			<div className='lantern lanterntag_container lanternten'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>光電股</div>
 			</div>
-			<div className='lantern lanterneleven'>
+			<div className='lantern lanterntag_container lanterneleven'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>資訊服務股</div>
 			</div>
-			<div className='lantern lanterntwelve'>
+			<div className='lantern lanterntag_container lanterntwelve'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>電子零件股</div>
 			</div>
-			<div className='lantern lanternthirteen'>
+			<div className='lantern lanterntag_container lanternthirteen'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>航運股</div>
 			</div>
-			<div className='lantern lanternforteen'>
+			<div className='lantern lanterntag_container lanternforteen'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>金融股</div>
 			</div>
-			<div className='lantern lanternfifteen'>
+			<div className='lantern lanterntag_container lanternfifteen'>
 				<div className='laternlight'></div>
 				<div className='rounded-t-lg left rounded-b-md'></div>
 				<div className='rounded-t-lg right rounded-b-md'></div>
 				<div className='flame'></div>
+				<div className='flex justify-center w-8 mx-2 my-5 laterntag'>綠能環保股</div>
 			</div>
 		</div>
 	)
