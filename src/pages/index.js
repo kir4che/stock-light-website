f1487fe4af66a29bc02bd8fa69dbf84d4bdeba82
kir4che <mollydcxxiii@gmail.com>
2023-10-03 17:08:46 +0800

    remove: Homepage old version

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,16 +106,6 @@ export default function Home() {
 					</LanternLayout>
 				</article>
 			</div>
-			{/*
-        前一版本舊有的內容
-      <Marquee />
-			<div className='pt-8 pb-12 sm:pt-12 sm:pb-16'>
-				<div className='flex w-full md:gap-12 lg:gap-20 xl:gap-24'>
-					<Sidebar />
-					<MainContent />
-				</div>
-			</div>
-      */}
 		</>
 	)
 }
