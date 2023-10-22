f8b46c5f0e7e0d3762f3241e350f384dd423defb
kir4che <mollydcxxiii@gmail.com>
2023-10-22 16:59:01 +0800

    Update index.js

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,8 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-import Loading from '@/components/common/Loading'
-import NewsPost from '@/components/News/NewsPost'
 import NewsSidebar from '@/components/News/NewsSidebar'
 import PaginationLink from '@/components/News/PaginationLink'
 
@@ -65,6 +63,7 @@ export default function News() {
 
 		// 從 localStorage 中取出上個月更新的熱門新聞
 		const storedHotNews = localStorage.getItem('hotNews')
+		console.log('storedHotNews', storedHotNews)
 		if (storedHotNews) setHotNews(JSON.parse(storedHotNews))
 		else setHotNews(currentDate)
 
@@ -93,7 +92,7 @@ export default function News() {
 	return (
 		<div className='flex flex-col items-center px-4 pt-10 pb-8 md:px-0'>
 			<div className='flex w-full md:gap-12 xl:gap-24'>
-				{!isLoading ? (
+				{/* {!isLoading ? (
 					<div className='w-full space-y-10'>
 						{newsByKeyword && newsByKeyword.length > 0 ? (
 							newsByKeyword.map((news, index) => <NewsPost news={news} key={index} />)
@@ -105,7 +104,7 @@ export default function News() {
 					</div>
 				) : (
 					<Loading />
-				)}
+				)} */}
 				{hotNews && hotNews.length > 0 && <NewsSidebar hotNews={hotNews} setNewsByKeyword={setNewsByKeyword} />}
 			</div>
 			{!newsByKeyword ? <PaginationLink totalPages={10} /> : null}
