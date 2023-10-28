ac8927a63d7d0eca6bc8f4d4dde3b557cab67dc1
Wojolan <109399275+aa901002x@users.noreply.github.com>
2023-10-29 01:37:20 +0800

    修改卡片歷史排版

--- src/pages/user/[id]/cardHistory/index.js
+++ src/pages/user/[id]/cardHistory/index.js
@@ -3,20 +3,61 @@ import StarryBackground from '@/components/common/StarryBackground'
 
 export default function cardHistory() {
     return [
-        <StarryBackground className={'pt-12 pb-9 sm:pt-20 sm:pb-16 text-zinc-100'}>
-            <section class="card-section"></section>
-            <div class="card-grid">
-                <a class="card" href="#">
-                    <div class="card__background" >
-                        <img src='https://img.lovepik.com/photo/40147/0563.jpg_wh300.jpg' alt='' />
-                    </div>
-                    <div class="card__content">
-                        <p class="card__date">2023年10月28日</p>
-                        <h3 class="card__category">金融類股</h3>
-                    </div>
-                </a>
-            </div>
- 
+        <StarryBackground className={'pt-12 pb-3 sm:pt-20 sm:pb-16 text-zinc-100'}>
+            <section className='flex flex-row items-start min-h-full justify-center' >
+                <div className="card-grid gap-10">
+                    <a className="list-none relative card" href="#">
+                        <div className="card__background" >
+                            <img src='https://img.lovepik.com/photo/40147/0563.jpg_wh300.jpg' alt='' />
+                        </div>
+                        <div className="left-0 top-0 absolute">
+                            <p className="text-sm uppercase">2023年10月28日</p>
+                            <h3 className="card__category">金融類股</h3>
+                        </div>
+                    </a>
+
+                    <a className="list-none relative card" href="#">
+                        <div className="card__background" >
+                            <img src='https://fakeimg.pl/300x172' alt='' />
+                        </div>
+                        <div className="left-0 top-0 absolute">
+                            <p className="text-sm uppercase">2023年10月28日</p>
+                            <h3 className="card__category">金融類股</h3>
+                        </div>
+                    </a>
+
+                    <a className="list-none relative card" href="#">
+                        <div className="card__background" >
+                            <img src='https://fakeimg.pl/300x172' alt='' />
+                        </div>
+                        <div className="left-0 top-0 absolute">
+                            <p className="text-sm uppercase">2023年10月28日</p>
+                            <h3 className="card__category">金融類股</h3>
+                        </div>
+                    </a>
+
+                    <a className="list-none relative card" href="#">
+                        <div className="card__background" >
+                            <img src='https://fakeimg.pl/300x172' alt='' />
+                        </div>
+                        <div className="left-0 top-0 absolute">
+                            <p className="text-sm uppercase">2023年10月28日</p>
+                            <h3 className="card__category">金融類股</h3>
+                        </div>
+                    </a>
+
+                    <a className="list-none relative card" href="#">
+                        <div className="card__background" >
+                            <img src='https://fakeimg.pl/300x172' alt='' />
+                        </div>
+                        <div className="left-0 top-0 absolute">
+                            <p className="text-sm uppercase">2023年10月28日</p>
+                            <h3 className="card__category">金融類股</h3>
+                        </div>
+                    </a>
+
+                </div>
+            </section>
         </StarryBackground>
     ]
 }
