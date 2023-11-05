76deaeac657956426c48778afd9278f1551bf25a
Wojolan <109399275+aa901002x@users.noreply.github.com>
2023-11-05 15:05:28 +0800

    修改隨機卡片

--- src/components/Light/PrayerCard.js
+++ src/components/Light/PrayerCard.js
@@ -4,9 +4,10 @@ import { cardList } from '@/data/cardList'
 import { useEffect } from 'react'
 
 export default function PrayerCard() {
+	const randomId = Math.floor(Math.random() * cardList.length);
+	const setselectedCard = cardList[randomId];
 
-	// const randomId = Math.floor(Math.random() * cardList.length);
-	// const setselectedCard = randomId;
+	
 
 	useEffect(() => {
 		//...
@@ -14,10 +15,12 @@ export default function PrayerCard() {
 
 	return (
 		<>
-			{
+			<Image src={setselectedCard.img_url} width={680} height={360} alt={setselectedCard.id} key={setselectedCard.id} />
+
+			{/* {
 				cardList.map((card) => (
 					<Image src={card.img_url} width={680} height={360} alt={card.id} key={card.id} />
-				))}
+				))} */}
 		</>
 	)
 }
--- src/data/cardList.js
+++ src/data/cardList.js
@@ -4,4 +4,12 @@ export const cardList = [
 		id: 1,
 		img_url: 'https://imgur.com/h4ICpmE.png',
 	},
+	{
+		id: 2,
+		img_url: 'https://i.imgur.com/bZOT3Sq.png',
+	},
+	{
+		id: 3,
+		img_url: 'https://i.imgur.com/odLPGpp.png',
+	},
 ]
