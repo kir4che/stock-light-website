622da2629cfbc9e54fe2da843a9bfd0d3d92a531
Wojolan <109399275+aa901002x@users.noreply.github.com>
2023-11-19 12:18:36 +0800

    Update: 新增天氣說明

--- src/pages/analysis/weather/index.js
+++ src/pages/analysis/weather/index.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { Popover } from '@arco-design/web-react';
 
 import Loading from '@/components/common/Loading'
 import StarryBackground from '@/components/common/StarryBackground'
 import StockSelect from '@/components/ui/StockSelector'
 import { allStock } from '@/data/allStock'
+import Chart from '@/components/Chart/Chart'
 
 const WeatherSidebar = [
 	{
@@ -11,18 +13,21 @@ const WeatherSidebar = [
 		en_name: 'Sunny',
 		ch_name: '晴天',
 		icon: 'https://img.icons8.com/emoji/32/sun-emoji.png',
+		desc: "雲量佔全天空0~4/10",
 	},
 	{
 		id: 2,
 		en_name: 'Cloudy',
 		ch_name: '多雲',
 		icon: 'https://img.icons8.com/office/32/partly-cloudy-day--v1.png',
+		desc: "雲量佔全天空5/10~8/10",
 	},
 	{
 		id: 3,
 		en_name: 'Rainny',
 		ch_name: '雨天',
 		icon: 'https://img.icons8.com/external-filled-outline-lima-studio/32/external-rainny-spring-filled-outline-lima-studio.png',
+		desc: "從層狀雲降落，降雨強度均勻緩和之連續或間歇性降雨",
 	},
 	{
 		id: 4,
@@ -60,6 +65,7 @@ export default function WeatherAnalysis() {
 			})
 			const data = await response.json()
 			console.log('data', data)
+			setWeatherData(data)
 			setIsLoading(false)
 		} catch (error) {
 			console.error('error', error)
@@ -71,6 +77,8 @@ export default function WeatherAnalysis() {
 		fetchWeatherPredict(selectedWeatherType, selectedStockSymbol)
 	}, [selectedStockSymbol, selectedWeatherType])
 
+
+
 	return (
 		<StarryBackground className={'w-full pt-8 pb-12 md:pt-10'}>
 			<h2 className='mb-4 text-center text-zinc-100'>天氣相關性分析</h2>
@@ -78,9 +86,8 @@ export default function WeatherAnalysis() {
 				<section className='w-full py-4 space-y-1 bg-white rounded md:w-1/5 dark:bg-zinc-900/50'>
 					{WeatherSidebar.map((weather, index) => (
 						<button
-							className={`flex items-center space-x-1.5 w-full px-3 py-2 hover:bg-primary_yellow/20 ${
-								selectedTab === index ? 'bg-primary_yellow/50 dark:bg-primary_yellow/30' : ''
-							}`}
+							className={`flex items-center space-x-1.5 w-full px-3 py-2 hover:bg-primary_yellow/20 ${selectedTab === index ? 'bg-primary_yellow/50 dark:bg-primary_yellow/30' : ''
+								}`}
 							key={index}
 							onClick={() => {
 								setSelectedWeatherType(weather.en_name.toLowerCase())
@@ -99,9 +106,15 @@ export default function WeatherAnalysis() {
 								<span>{allStock.find((stock) => stock.symbol === selectedStockSymbol).name || null}</span>
 								<span className='text-xl font-light tracking-widest'>{selectedStockSymbol}</span>
 							</h3>
-							<p className='px-2.5 py-1 text-xs text-white rounded-full bg-secondary_blue'>
-								{WeatherSidebar[selectedTab].ch_name}
-							</p>
+							<Popover
+								triggerEvent="hover"
+								title={WeatherSidebar[selectedTab].ch_name}
+								className={"text-black bg-white rounded-lg font-bold opacity-60 px-2 "}
+								content={<div><hr></hr>{WeatherSidebar[selectedTab].desc}</div>}>
+								<p className='px-2.5 py-1 text-xs text-white rounded-full bg-secondary_blue'>
+									{WeatherSidebar[selectedTab].ch_name}
+								</p>
+							</Popover>
 						</div>
 						<StockSelect value={selectedStockSymbol} onChange={(e) => setSelectedStockSymbol(e.target.value)} />
 					</div>
