a0070729c07dc793f97703d3cb6b478c66fbaac1
kir4che <mollydcxxiii@gmail.com>
2023-11-25 19:54:41 +0800

    刪除不必要的

--- src/pages/version/index.js
+++ src/pages/version/index.js
@@ -1,13 +1,36 @@
 import Image from 'next/image'
 
-import { versionInfo } from '@/data/versionInfo'
-
 export default function Version() {
+	const info = [
+		{
+			title: 'Version 2.0.0',
+			url: '/version',
+			image: [
+				'/assets/version/version2.0.0-1.png',
+				'/assets/version/version2.0.0-2.png',
+				'/assets/version/version2.0.0-3.png',
+			],
+			desc: '更換網站風格、新增點燈 / 股市...各項功能',
+		},
+		{
+			title: 'Version 1.1.0',
+			url: '/version',
+			image: ['/assets/version/version1.1.0.png'],
+			desc: 'banner 動畫設計',
+		},
+		{
+			title: 'Version 1.0.0',
+			url: '/version',
+			image: ['/assets/version/version1.0.0.png'],
+			desc: '確認網站的架構以及主色調',
+		},
+	]
+
 	return (
 		<div className='max-w-screen-sm px-8 pt-12 pb-20 mx-auto lg:px-0'>
 			<h2 className='mb-5 text-center pb-2.5 border-b-[3px] border-b-primary_yellow'>版本更新資訊</h2>
 			<ul className='space-y-3'>
-				{versionInfo.map((item) => (
+				{info.map((item) => (
 					<li key={item.version}>
 						<h5 className='font-bold text-center'>{item.title}</h5>
 						{item.image.map((imgSrc, index) => (
