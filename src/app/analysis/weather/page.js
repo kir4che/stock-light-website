3cb0ea3902cdc18e9e820de01f64945dd89dafd9
kir4che <mollydcxxiii@gmail.com>
2023-12-07 10:02:19 +0800

    fix: weather

--- src/app/analysis/weather/page.js
+++ src/app/analysis/weather/page.js
@@ -32,7 +32,7 @@ export default function WeatherAnalysis() {
 	const [isLoading, setIsLoading] = useState(true)
 
 	const [selectedTab, setSelectedTab] = useState(0)
-	const [selectedStockId, setSelectedStockId] = useState(2330)
+	const [selectedStockId, setSelectedStockId] = useState(1101)
 	const [selectedWeatherType, setSelectedWeatherType] = useState('sunny')
 
 	const [weatherData, setWeatherData] = useState({
@@ -130,7 +130,7 @@ export default function WeatherAnalysis() {
 							disablePortal
 						/>
 					</section>
-					{!isLoading && chartData && weatherData ? (
+					{!isLoading && chartData && weather && stockPrice ? (
 						<section className='flex flex-wrap items-start space-y-4'>
 							<Chart
 								option={linearRegOption(stockName, weatherType, weather, stockPrice)}
