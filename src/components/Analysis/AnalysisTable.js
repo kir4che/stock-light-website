e6876e440c83ab65d44e9587a00f77be77889e12
kir4che <mollydcxxiii@gmail.com>
2023-12-12 00:52:46 +0800

    Update AnalysisTable.js

--- src/components/Analysis/AnalysisTable.js
+++ src/components/Analysis/AnalysisTable.js
@@ -1,3 +1,4 @@
+import sentiments from '@/data/sentiments.json'
 import CloseIcon from '@mui/icons-material/Close'
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer'
 import { Dialog, DialogContent, DialogTitle } from '@mui/material'
@@ -38,11 +39,10 @@ export default function AnalysisTable({ stockId }) {
 			const response = await fetch(`${process.env.DB_URL}/api/stock/sentiment_analysis/${stockId}`, {
 				method: 'GET',
 			})
-			console.log(response) // test
 			const data = await response.json()
 			console.log(data) // test
 
-			// if (!data.success) setSentimentData(sentiments.rows.filter((item) => item.stock_id === stockId))
+			if (!data.success) setSentimentData(sentiments.rows.filter((item) => item.stock_id === stockId))
 
 			const filteredData = data.data.filter((item) => item.stock_id === stockId)
 
