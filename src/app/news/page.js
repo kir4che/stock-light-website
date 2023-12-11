23cb8ac50287cf88fca4993d81e399da63899363
kir4che <mollydcxxiii@gmail.com>
2023-12-11 21:45:59 +0800

    Update: News

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,6 +1,10 @@
 'use client'
 
+import CloseIcon from '@mui/icons-material/Close'
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import SearchIcon from '@mui/icons-material/Search'
+import InputBase from '@mui/material/InputBase'
+import Paper from '@mui/material/Paper'
 import { useEffect, useState } from 'react'
 
 import Loading from '@/components/common/Loading'
@@ -34,9 +38,14 @@ export default function News() {
 		}
 	}
 
+	const handleNewsSearch = () => {
+		const filteredNews = allNews.filter((news) => news.title.toLowerCase().includes(keyword.toLowerCase()))
+		setAllNews(filteredNews)
+	}
+
 	useEffect(() => {
-		fetchAllNews()
-	}, [])
+		if (keyword === '') fetchAllNews()
+	}, [keyword])
 
 	return (
 		<div className='flex flex-col px-4 py-8 md:px-0'>
@@ -46,7 +55,30 @@ export default function News() {
 					{!isLoading ? allNews.map((news, index) => <NewsPost news={news} key={index} />) : <Loading />}
 				</section>
 				<section className='hidden space-y-8 w-96 md:block'>
-					{/* <SearchInput allNews={allNews} keyword={keyword} setKeyword={setKeyword} setNews={setAllNews} /> */}
+					<Paper component='form' className='flex relative items-center pl-3 py-1.5'>
+						<InputBase
+							placeholder='Search'
+							value={keyword}
+							onChange={(e) => setKeyword(e.target.value)}
+							onKeyDown={(e) => {
+								if (e.key === 'Enter') {
+									e.preventDefault()
+									handleNewsSearch()
+								}
+							}}
+						/>
+						{keyword && (
+							<CloseIcon
+								fontSize='small'
+								className='absolute mr-1 cursor-pointer text-zinc-800 dark:text-zinc-800 right-9 hover:opacity-80'
+								onClick={() => setKeyword('')}
+							/>
+						)}
+						<SearchIcon
+							className='absolute cursor-pointer text-zinc-800 dark:text-zinc-800 right-3 hover:opacity-80'
+							onClick={() => handleNewsSearch}
+						/>
+					</Paper>
 					<SidebarBlock icon={<RocketLaunchIcon className='dark:text-white' />} title={'今日熱點'} />
 				</section>
 			</div>
