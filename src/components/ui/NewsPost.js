db196ae11e20c2afe060d6386a6c34dcca7032da
kir4che <mollydcxxiii@gmail.com>
2023-12-16 20:15:26 +0800

    Update NewsPost.js

--- src/components/ui/NewsPost.js
+++ src/components/ui/NewsPost.js
@@ -22,18 +22,12 @@ export default function NewsPost({ news }) {
 					<Link href={news.link} target='_blank' className='hover:text-zinc-500 dark:hover:text-zinc-300'>
 						<h4>{news.title}</h4>
 					</Link>
-					<p className='space-x-2 text-xs flex-center text-zinc-500/80 dark:text-zinc-200/50'>
+					<p className='flex space-x-3 text-xs item-center text-zinc-500/80 dark:text-zinc-200/50'>
 						<span className='space-x-1 flex-center'>
 							{news.source_icon && (
-								<Image
-									src={news.source_icon}
-									width={100}
-									height={100}
-									className='object-cover xs:max-h-8'
-									alt='news-icon'
-								/>
+								<Image src={news.source_icon} width={100} height={100} className='object-cover h-8' alt='news-icon' />
 							)}
-							{news.source_name}
+							<span>{news.source_name}</span>
 						</span>
 						<span>{convertDateTime(news.time)}</span>
 					</p>
