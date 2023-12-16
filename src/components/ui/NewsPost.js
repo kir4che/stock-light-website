4d69ca619e903395f1e4d25bf9790a9b262079b2
kir4che <mollydcxxiii@gmail.com>
2023-12-16 20:21:53 +0800

    Update NewsPost.js

--- src/components/ui/NewsPost.js
+++ src/components/ui/NewsPost.js
@@ -25,7 +25,13 @@ export default function NewsPost({ news }) {
 					<p className='flex items-center space-x-3 text-xs text-zinc-500/80 dark:text-zinc-200/50'>
 						<span className='space-x-1 flex-center'>
 							{news.source_icon && (
-								<Image src={news.source_icon} width={100} height={100} className='object-contain h-6' alt='news-icon' />
+								<Image
+									src={news.source_icon}
+									width={100}
+									height={100}
+									className='object-contain h-6 w-fit'
+									alt='news-icon'
+								/>
 							)}
 							<span>{news.source_name}</span>
 						</span>
