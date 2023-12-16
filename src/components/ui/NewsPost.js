50ffcb614fda549807a78bd739ef6837856ca803
kir4che <mollydcxxiii@gmail.com>
2023-12-16 15:37:33 +0800

    Update NewsPost.js

--- src/components/ui/NewsPost.js
+++ src/components/ui/NewsPost.js
@@ -2,27 +2,24 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NewsPost({ news }) {
-	const coverImg = news.imageSrc.split(',')[0].replace(/1x$/, '').trim()
-
 	return (
 		<article className='gap-6 space-y-2 xs:space-y-0 xs:flex'>
 			<div className='xs:max-w-[225px] h-full w-full'>
-				{coverImg && (
-					<Image
-						src={coverImg}
-						width={400}
-						height={400}
-						className='xs:max-h-[145px] object-cover'
-						alt={news.news_id || 'news-cover'}
-					/>
-				)}
+				<Image
+					src={news.imageSrc}
+					width={400}
+					height={400}
+					className='xs:max-h-[145px] object-cover'
+					alt={news.news_id || 'news-cover'}
+				/>
 			</div>
 			<div className='space-y-1.5'>
 				<Link href={news.link} target='_blank' className='hover:text-zinc-500 dark:hover:text-zinc-300'>
 					<h4>{news.title}</h4>
 				</Link>
 				<p className='text-xs text-zinc-500/80 dark:text-zinc-200/50'>
-					{news.update_date && new Date(news.update_date).toISOString().split('T')[0] + ' ｜ '}
+					<Image src={news.source_icon} width={100} height={100} className='object-cover xs:max-h-20' alt='news-icon' />
+					{news.source_name}
 					{news.time}
 				</p>
 			</div>
