efe270b7fc28d07014df4c4d6eb0a00f73235946
kir4che <mollydcxxiii@gmail.com>
2023-12-17 20:29:27 +0800

    Update page.js

--- src/app/light/resultDashboard/page.js
+++ src/app/light/resultDashboard/page.js
@@ -5,6 +5,7 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp'
 import { Tab, Tabs } from '@mui/material'
 import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
 import { Suspense, useEffect, useState } from 'react'
 
 import AnalysisTable from '@/components/Light/AnalysisTable'
@@ -20,6 +21,7 @@ import fetchStockData from '@/utils/fetchStockData'
 import fetchStockPePb from '@/utils/fetchStockPePb'
 
 function ResultDashboard() {
+	const router = useRouter()
 	const industry = useSearchParams().get('industry')
 	const factor = useSearchParams().get('factor')
 
@@ -80,6 +82,13 @@ function ResultDashboard() {
 	}
 
 	useEffect(() => {
+		router.onError = (err) => {
+			const pattern = /Loading chunk (\d)+ failed/g
+			const isChunkLoadFailed = err.message.match(pattern)
+			const targetPath = router.history.pending.fullPath
+			if (isChunkLoadFailed) router.replace(targetPath)
+		}
+
 		fetchStockByFactor()
 	}, [])
 
