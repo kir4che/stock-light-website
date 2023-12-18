f212fcba677b25f6f49ad74927cc1a4d075cef7e
kir4che <mollydcxxiii@gmail.com>
2023-12-18 12:49:56 +0800

    Update page.js

--- src/app/user/[id]/cardHistory/page.js
+++ src/app/user/[id]/cardHistory/page.js
@@ -16,6 +16,7 @@ export default function CardHistory() {
 			})
 
 			const data = await response.json()
+			console.log(data)
 			if (data.success) setCards(data.data)
 		} catch (error) {
 			console.error('Error: ', error)
