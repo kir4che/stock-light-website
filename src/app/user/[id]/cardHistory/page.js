8b01c07cd7212c4c7f7c29ded83ebb48a462230c
kir4che <mollydcxxiii@gmail.com>
2024-01-10 00:47:00 +0800

    fix card

--- src/app/user/[id]/cardHistory/page.js
+++ src/app/user/[id]/cardHistory/page.js
@@ -37,20 +37,18 @@ export default function CardHistory() {
 	return (
 		<StarryBackground className='w-full px-4 pt-6 lg:pt-12 md:px-8 text-zinc-100'>
 			<Breadcrumbs prevPage='會員管理' curPage='我的小卡' />
-			<section className='flex flex-row items-start min-h-full mt-6'>
+			<section className='grid grid-cols-1 gap-4 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 				{cards.length > 0 ? (
 					cards.map((card) => (
-						<div className='gap-10 card-grid'>
-							<a href={card.image_link} target='_blank' className='relative list-none card'>
-								<div className='card__background'>
-									<Image src={card.image_link} width={640} height={320} alt='card' />
-								</div>
-								<div className='absolute top-0 left-0 p-2'>
-									<p className='text-sm uppercase'>{new Date(card.create_date).toISOString().split('T')[0]}</p>
-									<h3 className='card__category'>{card.type}</h3>
-								</div>
-							</a>
-						</div>
+						<a href={card.image_link} target='_blank' className='relative list-none card'>
+							<div className='card__background'>
+								<Image src={card.image_link} width={640} height={320} alt='card' />
+							</div>
+							<div className='absolute top-0 left-0 p-2'>
+								<p className='text-sm uppercase'>{new Date(card.create_date).toISOString().split('T')[0]}</p>
+								<h3 className='card__category'>{card.type}</h3>
+							</div>
+						</a>
 					))
 				) : (
 					<p>暫無小卡，快到點燈系統保存一張吧！</p>
