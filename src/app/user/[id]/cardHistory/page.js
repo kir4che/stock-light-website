f880f1abce75d9aa261068f4b64e95921aec19db
kir4che <mollydcxxiii@gmail.com>
2024-01-10 01:02:56 +0800

    fix card

--- src/app/user/[id]/cardHistory/page.js
+++ src/app/user/[id]/cardHistory/page.js
@@ -38,12 +38,12 @@ export default function CardHistory() {
 
 	useEffect(() => {
 		fetchCards()
-	}, [token])
+	}, [])
 
 	return (
 		<StarryBackground className='w-full px-4 pt-6 lg:pt-12 md:px-8 text-zinc-100'>
 			<Breadcrumbs prevPage='會員管理' curPage='我的小卡' />
-			{isLoading && (
+			{!isLoading && (
 				<section className='grid grid-cols-1 gap-4 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 					{cards.length > 0 ? (
 						cards.map((card) => (
